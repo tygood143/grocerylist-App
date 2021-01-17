6dc24cc9b35da78c92e341c5b55b4e8f990864fc
Tyler Good <tygood143@gmail.com>
2021-01-17 15:36:13 -0700

    Initial Commit

---
+++ grocerylist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grocerylist
+//
+//  Created by Tyler Good on 1/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ grocerylist/grocerylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocerylistApp.swift
+//  grocerylist
+//
+//  Created by Tyler Good on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct grocerylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
